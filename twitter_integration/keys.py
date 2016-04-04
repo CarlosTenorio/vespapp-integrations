93bdc702247943c480730a43c0de0490b4402c18
JoanMarMir <joanmartinmiralles@hotmail.com>
2016-04-04 17:09:11 +0200

    untracked some files

--- twitter_integration/keys.py
+++ twitter_integration/keys.py
@@ -1,6 +1,6 @@
 keys = dict(
-	consumer_key = 'qO2wzYilhvmXsRii2zIw3FM1Z',
-	consumer_secret = '1YddGeGfM3Wp53NFaGMkvXQwv9ek1colosRXx0nYLiUp72mbE4',
-	access_token = '707877442795532288-cnU1gAHKCCLRUbVg8q4e3ofzMSSq79b',
-	access_token_secret = 'apNn6kJu9Rq3W8j6TRAdgw2ZSfBys3uDDlmPNnEEvsSsF',
+	consumer_key = '***',
+	consumer_secret = '***',
+	access_token = '***',
+	access_token_secret = '***',
 )
